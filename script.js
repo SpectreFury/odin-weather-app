65b1dd604a38ae9d3140a05ce7ea5c692cbaf0a6
Ayush Soni <soni.ayush.2212@gmail.com>
2022-09-29 22:41:01 +0530

    Add different background for weathers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input = document.querySelector("input");
-const temperatureContainer = document.querySelector(".temperature-container");
+const cardInner = document.querySelector(".card-inner");
 const cityName = document.querySelector(".city-name");
 const cityCountry = document.querySelector(".city-country");
 const timeDisplay = document.querySelector(".time-display");
@@ -39,8 +39,28 @@ const appendWeather = function (data) {
   temperature.textContent = (data.main.temp - 273.15).toFixed();
   description.textContent = data.weather[0].main;
 
-  wind.textContent = `${data.wind.speed}m/s`
+  if (description.textContent === "Rain") {
+    cardInner.style.background =
+      "url(https://media2.giphy.com/media/DD4FroTT30PeSamZbG/giphy.gif?cid=52b5febdgmh568ihn4gnu3yazifhlqei4xd0mm39m3ohhqi5&rid=giphy.gif&ct=g)";
+  } else if (description.textContent === "Clouds") {
+    cardInner.style.background =
+      "url(https://media0.giphy.com/media/3o7rc6sa2RvKo8K5EI/giphy.gif?cid=ecf05e47069ipjw9ysnjm07gku85fza72jmjud7p0jq7rk7p&rid=giphy.gif&ct=g) no-repeat center / cover";
+  } else if (description.textContent === "Snow") {
+    cardInner.style.background =
+      "url(https://media2.giphy.com/media/6YNgoTEPs6vZe/giphy.gif?cid=ecf05e473zty1j5jz195u4ylcnnwfqexux49w8rc6royjhab&rid=giphy.gif&ct=g)";
+  } else if (description.textContent === "Clear") {
+    cardInner.style.background =
+      "url(https://media0.giphy.com/media/wNipYAoZ3iaEE/giphy.gif?cid=ecf05e47488k93p8btyi71go24pq7mctye0r0tuakc8rhy43&rid=giphy.gif&ct=g)";
+  } else if (description.textContent === "Thunderstorm") {
+    cardInner.style.background =
+      "url(https://media2.giphy.com/media/3osxYzIQRqN4DOEddC/giphy.gif?cid=ecf05e479crwj5jdf5v6xlybg1q8gkmnznk74z0qqmhy6k2o&rid=giphy.gif&ct=g)";
+  } else {
+    cardInner.style.background =
+      "url(https://media1.giphy.com/media/1zgzISaYrnMAYRJJEr/giphy.gif?cid=ecf05e47msa4jyggxjqv7ly7fqoz3sogpyxs722nn5w53vyz&rid=giphy.gif&ct=g) no-repeat center / cover";
+  }
+
+  wind.textContent = `${data.wind.speed}m/s`;
   visibility.textContent = `${(data.visibility / 1000).toFixed(1)}km`;
   pressure.textContent = `${data.main.pressure}hPa`;
-  humidity.textContent = `${data.main.humidity}%`
+  humidity.textContent = `${data.main.humidity}%`;
 };
