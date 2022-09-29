4a7c3c5580b87368481870fb9c58f247a7357718
Ayush Soni <soni.ayush.2212@gmail.com>
2022-09-30 00:25:09 +0530

    Add city not found modal

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const input = document.querySelector("input");
+const inputGroup = document.querySelector(".input-group");
 const cardInner = document.querySelector(".card-inner");
 const cityName = document.querySelector(".city-name");
 const cityCountry = document.querySelector(".city-country");
@@ -9,6 +10,7 @@ const wind = document.querySelector(".detail-wind");
 const visibility = document.querySelector(".detail-visibility");
 const pressure = document.querySelector(".detail-pressure");
 const humidity = document.querySelector(".detail-humidity");
+const modalError = document.querySelector(".modal-error");
 
 input.addEventListener("keydown", async (e) => {
   if (e.key === "Enter") {
@@ -16,6 +18,12 @@ input.addEventListener("keydown", async (e) => {
       return;
     }
     const data = await fetchWeather(input.value);
+    if (+data.cod === 404) {
+      modalError.classList.add("show");
+      setTimeout(() => {
+        modalError.classList.remove('show');
+      }, 2000);
+    }
 
     appendWeather(data);
   }
